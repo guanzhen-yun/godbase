dc58e7c303c9cc94894dcde4d60c376ca66b2a91
guanz1 <guanz1@ziroom.com>
2020-05-11 21:57:11 +0800

    提交代码

--- net/src/main/java/com/ziroom/net/rx/ServerNoBodyFunc.java
+++ net/src/main/java/com/ziroom/net/rx/ServerNoBodyFunc.java
@@ -15,12 +15,12 @@ public class ServerNoBodyFunc implements Function<Result, Result> {
     @Override
     public Result apply(Result httpResult) {
         if (!TextUtils.isEmpty(httpResult.getErrcode())) {
-            if (!"0".equals(httpResult.getErrcode()) && !"0.0".equals(httpResult.getErrcode())) {
+            if (!"0".equals(httpResult.getErrcode()) && !"0.0".equals(httpResult.getErrcode()) && !"1000".equals(httpResult.getErrcode())) {
                 throw new ServerException(Integer.parseInt(httpResult.getErrcode()), Integer.parseInt(httpResult.getErrcode()), httpResult.getErrmsg());
             } else {
                 return httpResult;
             }
-        } else if (httpResult.getError() == 0 && httpResult.getCode() == 200) {
+        } else if (httpResult.getError() == 0 && (httpResult.getCode() == 1000 || httpResult.getCode() == 0 || httpResult.getCode() == 200)) {
             return httpResult;
         } else {
             throw new ServerException(httpResult.getCode(), httpResult.getError(), httpResult.getMessage());
