2bc2373a868ff038582fc7767eac98604d1ae6f8
guanzhen <guanzhen@inke.cn>
2021-07-22 17:10:51 +0800

    网络库升级

--- app/src/main/java/com/ziroom/godbase/App.java
+++ app/src/main/java/com/ziroom/godbase/App.java
@@ -3,6 +3,7 @@ package com.ziroom.godbase;
 import android.app.Application;
 
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.ziroom.godbase.service.ChangeEnvironmentManager;
 import com.ziroom.godbase.service.CommonParam;
 import com.ziroom.godbase.service.HeaderParam;
 import com.ziroom.godbase.service.Host;
@@ -31,7 +32,11 @@ public class App extends Application {
      * 初始化网络信息
      */
     public static void initNetInfo() {
-        LogUtils.debug = BuildConfig.DEBUG;
+        if(ChangeEnvironmentManager.getInstance().getIsDebug() != -1) {
+            LogUtils.debug = ChangeEnvironmentManager.getInstance().getIsDebug() == 1;
+        } else {
+            LogUtils.debug = BuildConfig.DEBUG;
+        }
         RetrofitManager.getInstance().setBaseUrl(Host.getCommonHost())
                 .setParams(HeaderParam.getHeaderParams(), CommonParam.getCommonParams()).init(Host.getHosts());
     }
--- app/src/main/java/com/ziroom/godbase/MainActivity.java
+++ app/src/main/java/com/ziroom/godbase/MainActivity.java
@@ -1,10 +1,12 @@
 package com.ziroom.godbase;
 
+import android.view.View;
 import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.ziroom.base.BaseActivity;
 import com.ziroom.godbase.routers.RouterConstants;
+import com.ziroom.godbase.service.ChangeEnvironmentManager;
 
 import butterknife.OnClick;
 
@@ -20,9 +22,13 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
         Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
     }
 
-    @OnClick(R.id.tv)
-    public void onViewClicked() {
-        mPresenter.sendMvpRequest();
+    @OnClick({R.id.tv, R.id.tv_exchange})
+    public void onViewClicked(View v) {
+        if(v.getId() == R.id.tv) {
+            mPresenter.sendMvpRequest();
+        } else {
+            ChangeEnvironmentManager.getInstance().saveCurrentEnviroment((1 - ChangeEnvironmentManager.getInstance().getIsDebug()));
+        }
     }
 
     @Override
