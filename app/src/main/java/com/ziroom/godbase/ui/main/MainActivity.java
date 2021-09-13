3d8cc524c66d1d61512fdffdb77c3bb8be5896a7
guanzhen <739144003@qq.com>
2021-09-13 16:29:44 +0800

    修改本地生成class文件逻辑

--- app/src/main/java/com/ziroom/godbase/ui/main/MainActivity.java
+++ app/src/main/java/com/ziroom/godbase/ui/main/MainActivity.java
@@ -2,6 +2,7 @@ package com.ziroom.godbase.ui.main;
 
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
@@ -25,27 +26,41 @@ import butterknife.OnClick;
 
 @Route(path = RouterConstants.App.Main)
 public class MainActivity extends BaseActivity<MainPresenter> implements MainContract.IView {
-    @BindView(R.id.et_content)
-    EditText mEtContent;
+    @BindView(R.id.et_address)
+    EditText mEtAddress;
+    @BindView(R.id.et_classname)
+    EditText mEtClassname;
+    @BindView(R.id.btn_createfile)
+    Button mBtnCreatefile;
 
-    @OnClick({R.id.tv, R.id.tv_config, R.id.tv_create})
+    @OnClick({R.id.tv, R.id.tv_config, R.id.tv_create, R.id.btn_createfile})
     public void onViewClicked(View v) {
         if (v.getId() == R.id.tv) {
             mPresenter.sendMvpRequest();
         } else if (v.getId() == R.id.tv_config) {
             RouterUtils.jump(RouterConstants.App.Config);
         } else if (v.getId() == R.id.tv_create) {
-            String s = mEtContent.getText().toString();
-            if(!TextUtils.isEmpty(s)) {
-                if(!s.contains(",")) {
-                    ToastUtils.showShortToast("类绝对地址或者类名应以,拼接");
-                } else {
-                    String address = s.split(",")[0];
-                    String className = s.split(",")[1];
-                    mPresenter.createFileRequest(address, className);
-                }
+            setTestData();
+            mEtAddress.setVisibility(View.VISIBLE);
+            mEtClassname.setVisibility(View.VISIBLE);
+            mBtnCreatefile.setVisibility(View.VISIBLE);
+        } else if(v.getId() == R.id.btn_createfile) {
+            String address = mEtAddress.getText().toString();
+            String className = mEtClassname.getText().toString();
+            if (TextUtils.isEmpty(address) || TextUtils.isEmpty(className)) {
+                ToastUtils.showShortToast("类绝对地址和类名不能为空");
+            } else {
+                mPresenter.createFileRequest(address, className);
             }
         }
+    }
+
+    /**
+     * 测试数据
+     */
+    private void setTestData() {
+        mEtAddress.setText("/Users/guanzhen/godbase/app/src/main/java/com/ziroom/godbase/ui");
+        mEtClassname.setText("TestActivity");
     }
 
     @Override
@@ -71,5 +86,10 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
     @Override
     public void createFileResult(FileDo fileDo) {
         ToastUtils.showShortToast(fileDo.getFileResult());
+        mEtAddress.setVisibility(View.GONE);
+        mEtClassname.setVisibility(View.GONE);
+        mEtAddress.setText("");
+        mEtClassname.setText("");
+        mBtnCreatefile.setVisibility(View.GONE);
     }
 }
