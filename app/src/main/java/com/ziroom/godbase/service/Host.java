431eaa328342ae33848eb0cf910d676251de2d59
关震 <guanzhen@helliogroup.com>
2023-02-16 16:57:09 +0800

    aaa

--- app/src/main/java/com/ziroom/godbase/service/Host.java
+++ app/src/main/java/com/ziroom/godbase/service/Host.java
@@ -12,35 +12,37 @@ import java.util.Map;
  * 域名映射
  */
 public class Host {
-    private static final String commonHostKey = "def";
-    private static final String otherHostKey = "other";
 
-    public static final String commonHostDomain = "Domain-Name: " + commonHostKey;
+  private static final String commonHostKey = "def";
+  private static final String otherHostKey = "other";
 
-    public static final String commonHostOther = "Domain-Name: " + otherHostKey;
+  public static final String commonHostDomain = "Domain-Name: " + commonHostKey;
 
-    //测试环境
-    public static final String commonHostDev = "http://192.168.2.12:8080/";
-    //正式环境
-    public static final String commonHostRelease = "http://api.abc.service/";
+  public static final String commonHostOther = "Domain-Name: " + otherHostKey;
 
-    public static final String DEV = "dev";//测试环境
-    public static final String RELEASE = "release";//正式环境
+  //测试环境
+  public static final String commonHostDev = "http://172.23.32.83:8080/";
+  //正式环境
+  public static final String commonHostRelease = "http://api.abc.service/";
 
-    public static String getCommonHost() {
-        String hostEnv = SharedUtils.getInstance().getString(SharedUtils.ENV);
-        return TextUtils.isEmpty(hostEnv) ? BuildConfig.DOMAIN_NAME : hostEnv;
-    }
+  public static final String DEV = "dev";//测试环境
+  public static final String RELEASE = "release";//正式环境
 
-    public static String getOtherHost() {
-        String otherHostDev = SharedUtils.getInstance().getString(SharedUtils.ENV_OTHER);
-        return TextUtils.isEmpty(otherHostDev) ? BuildConfig.DOMAIN_NAME_OTHER : otherHostDev;
-    }
+  public static String getCommonHost() {
+    String hostEnv = SharedUtils.getInstance().getString(SharedUtils.ENV);
+    return TextUtils.isEmpty(hostEnv) ? BuildConfig.DOMAIN_NAME : hostEnv;
+  }
 
-    private static final Map<String, String> mHosts = new HashMap<>();
-    public static Map<String, String> getHosts() {
-        mHosts.put(commonHostKey, getCommonHost());
-        mHosts.put(otherHostKey, getOtherHost());
-        return mHosts;
-    }
+  public static String getOtherHost() {
+    String otherHostDev = SharedUtils.getInstance().getString(SharedUtils.ENV_OTHER);
+    return TextUtils.isEmpty(otherHostDev) ? BuildConfig.DOMAIN_NAME_OTHER : otherHostDev;
+  }
+
+  private static final Map<String, String> mHosts = new HashMap<>();
+
+  public static Map<String, String> getHosts() {
+    mHosts.put(commonHostKey, getCommonHost());
+    mHosts.put(otherHostKey, getOtherHost());
+    return mHosts;
+  }
 }
--- app/src/main/java/com/ziroom/godbase/ui/main/MainActivity.java
+++ app/src/main/java/com/ziroom/godbase/ui/main/MainActivity.java
@@ -47,148 +47,156 @@ import okhttp3.Response;
 
 @Route(path = RouterConstants.App.Main)
 public class MainActivity extends BaseActivity<MainPresenter> implements MainContract.IView {
-    @BindView(R.id.et_address)
-    EditText mEtAddress;
-    @BindView(R.id.et_classname)
-    EditText mEtClassname;
-    @BindView(R.id.btn_createfile)
-    Button mBtnCreatefile;
 
+  @BindView(R.id.et_address)
+  EditText mEtAddress;
+  @BindView(R.id.et_classname)
+  EditText mEtClassname;
+  @BindView(R.id.btn_createfile)
+  Button mBtnCreatefile;
+
+  private String downloadFileName;//下载的文件名称
+
+  @Override
+  public void initViews() {
+    //运行时权限申请
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+          Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+    }
+  }
+
+  @OnClick({R.id.tv, R.id.tv_config, R.id.tv_create, R.id.btn_createfile, R.id.btn_download,
+      R.id.btn_upload})
+  public void onViewClicked(View v) {
+    if (v.getId() == R.id.tv) {
+      mPresenter.sendMvpRequest();
+    } else if (v.getId() == R.id.tv_config) {
+      RouterUtils.jump(RouterConstants.App.Config);
+    } else if (v.getId() == R.id.tv_create) {
+      setTestData();
+      mEtAddress.setVisibility(View.VISIBLE);
+      mEtClassname.setVisibility(View.VISIBLE);
+      mBtnCreatefile.setVisibility(View.VISIBLE);
+    } else if (v.getId() == R.id.btn_createfile) {
+      String address = mEtAddress.getText().toString();
+      String className = mEtClassname.getText().toString();
+      if (TextUtils.isEmpty(address) || TextUtils.isEmpty(className)) {
+        ToastUtils.showShortToast("类绝对地址和类名不能为空");
+      } else {
+        mPresenter.createFileRequest(address, className);
+      }
+    } else if (v.getId() == R.id.btn_download) {
+      //http://192.168.2.12:8080/file/classes2.dex
+      downloadFileName = "zheng.jpg";
+      downLoadFile("http://172.23.32.83:8088/myfile/" + downloadFileName,
+          getExternalCacheDir().getAbsolutePath());//需要配置tomcat的配置文件 映射关系 tomcat/conf/web.xml
+      //https://blog.csdn.net/amunamuna/article/details/123879755
+    } else if (v.getId() == R.id.btn_upload) {
+      File file = new File(Environment.getExternalStorageDirectory(), "zheng.jpg");
+      mPresenter.uploadFile(file);
+    }
+  }
+
+  Handler handler = new Handler(Looper.getMainLooper()) {
     @Override
-    public void initViews() {
-        //运行时权限申请
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
-        }
+    public void handleMessage(@NonNull Message msg) {
+      super.handleMessage(msg);
+      ToastUtils.showShortToast("文件下载成功");
+    }
+  };
+
+  /**
+   * 下载文件
+   *
+   * @param fileUrl     文件url
+   * @param destFileDir 存储目标目录
+   */
+  public void downLoadFile(String fileUrl, final String destFileDir) {
+    final File file = new File(destFileDir, downloadFileName);
+    if (file.exists()) {
+      ToastUtils.showShortToast("文件已下载");
+      return;
     }
 
-    @OnClick({R.id.tv, R.id.tv_config, R.id.tv_create, R.id.btn_createfile, R.id.btn_download, R.id.btn_upload})
-    public void onViewClicked(View v) {
-        if (v.getId() == R.id.tv) {
-            mPresenter.sendMvpRequest();
-        } else if (v.getId() == R.id.tv_config) {
-            RouterUtils.jump(RouterConstants.App.Config);
-        } else if (v.getId() == R.id.tv_create) {
-            setTestData();
-            mEtAddress.setVisibility(View.VISIBLE);
-            mEtClassname.setVisibility(View.VISIBLE);
-            mBtnCreatefile.setVisibility(View.VISIBLE);
-        } else if(v.getId() == R.id.btn_createfile) {
-            String address = mEtAddress.getText().toString();
-            String className = mEtClassname.getText().toString();
-            if (TextUtils.isEmpty(address) || TextUtils.isEmpty(className)) {
-                ToastUtils.showShortToast("类绝对地址和类名不能为空");
-            } else {
-                mPresenter.createFileRequest(address, className);
+    OkHttpClient mOkHttpClient = new OkHttpClient();
+    final Request request = new Request.Builder().url(fileUrl).build();
+    final Call call = mOkHttpClient.newCall(request);
+    call.enqueue(new Callback() {
+      @Override
+      public void onFailure(Call call, IOException e) {
+      }
+
+      @Override
+      public void onResponse(Call call, Response response) throws IOException {
+        InputStream is = null;
+        byte[] buf = new byte[2048];
+        int len = 0;
+        FileOutputStream fos = null;
+        try {
+          long total = response.body().contentLength();
+          long current = 0;
+          is = response.body().byteStream();
+          fos = new FileOutputStream(file);
+          while ((len = is.read(buf)) != -1) {
+            current += len;
+            fos.write(buf, 0, len);
+          }
+          fos.flush();
+          handler.sendEmptyMessage(0);
+        } catch (IOException e) {
+          e.printStackTrace();
+        } finally {
+          try {
+            if (is != null) {
+              is.close();
             }
-        } else if(v.getId() == R.id.btn_download) {
-            //http://192.168.2.12:8080/file/classes2.dex
-            downLoadFile("http://192.168.2.12:8080/file/classes2.dex",
-                    getExternalCacheDir().getAbsolutePath());//需要配置tomcat的配置文件 映射关系 tomcat/conf/server.xml
-        } else if(v.getId() == R.id.btn_upload) {
-            File file = new File(Environment.getExternalStorageDirectory(), "bigpng.jpg");
-            mPresenter.uploadFile(file);
-        }
-    }
-
-    Handler handler = new Handler(Looper.getMainLooper()) {
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            super.handleMessage(msg);
-            ToastUtils.showShortToast("文件下载成功");
-        }
-    };
-
-    /**
-     * 下载文件
-     * @param fileUrl 文件url
-     * @param destFileDir 存储目标目录
-     */
-    public void downLoadFile(String fileUrl, final String destFileDir) {
-        final File file = new File(destFileDir, "classes2.dex");
-        if (file.exists()) {
-            ToastUtils.showShortToast("文件已下载");
-            return;
-        }
-
-        OkHttpClient mOkHttpClient = new OkHttpClient();
-        final Request request = new Request.Builder().url(fileUrl).build();
-        final Call call = mOkHttpClient.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
+            if (fos != null) {
+              fos.close();
             }
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    });
+  }
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                InputStream is = null;
-                byte[] buf = new byte[2048];
-                int len = 0;
-                FileOutputStream fos = null;
-                try {
-                    long total = response.body().contentLength();
-                    long current = 0;
-                    is = response.body().byteStream();
-                    fos = new FileOutputStream(file);
-                    while ((len = is.read(buf)) != -1) {
-                        current += len;
-                        fos.write(buf, 0, len);
-                    }
-                    fos.flush();
-                    handler.sendEmptyMessage(0);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        if (is != null) {
-                            is.close();
-                        }
-                        if (fos != null) {
-                            fos.close();
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-    }
+  /**
+   * 测试数据
+   */
+  private void setTestData() {
+    mEtAddress.setText("/Users/guanzhen/godbase/app/src/main/java/com/ziroom/godbase/ui");
+    mEtClassname.setText("TestActivity");
+  }
 
-    /**
-     * 测试数据
-     */
-    private void setTestData() {
-        mEtAddress.setText("/Users/guanzhen/godbase/app/src/main/java/com/ziroom/godbase/ui");
-        mEtClassname.setText("TestActivity");
-    }
+  @Override
+  public int getLayoutId() {
+    return R.layout.activity_main;
+  }
 
-    @Override
-    public int getLayoutId() {
-        return R.layout.activity_main;
-    }
+  @Override
+  public MainPresenter getPresenter() {
+    return new MainPresenter(this);
+  }
 
-    @Override
-    public MainPresenter getPresenter() {
-        return new MainPresenter(this);
-    }
+  @Override
+  public boolean isSetDarkStatusFrontColor() {
+    return true;
+  }
 
-    @Override
-    public boolean isSetDarkStatusFrontColor() {
-        return true;
-    }
+  @Override
+  public void getMvpResult(String result) {
+    ToastUtils.showShortToast(result);
+  }
 
-    @Override
-    public void getMvpResult(String result) {
-        ToastUtils.showShortToast(result);
-    }
-
-    @Override
-    public void createFileResult(FileDo fileDo) {
-        ToastUtils.showShortToast(fileDo.getFileResult());
-        mEtAddress.setVisibility(View.GONE);
-        mEtClassname.setVisibility(View.GONE);
-        mEtAddress.setText("");
-        mEtClassname.setText("");
-        mBtnCreatefile.setVisibility(View.GONE);
-    }
+  @Override
+  public void createFileResult(FileDo fileDo) {
+    ToastUtils.showShortToast(fileDo.getFileResult());
+    mEtAddress.setVisibility(View.GONE);
+    mEtClassname.setVisibility(View.GONE);
+    mEtAddress.setText("");
+    mEtClassname.setText("");
+    mBtnCreatefile.setVisibility(View.GONE);
+  }
 }
